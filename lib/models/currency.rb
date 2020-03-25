4834a14c5bb36b1a6ffd6b8fa3477628a4dc8c76
kohrVid <kohrVid@zoho.com>
2020-03-25 22:05:47 +0000

    Ensure that new currency codes are unique
    Have added uniqueness validation to Currency#code so as to avoid the
    duplication in the database.

--- lib/models/currency.rb
+++ lib/models/currency.rb
@@ -1,5 +1,5 @@
 require 'active_record'
 
 class Currency < ActiveRecord::Base
-  validates :code, length: { is: 3 }
+  validates :code, length: { is: 3 }, uniqueness: true
 end
--- spec/models/currency_spec.rb
+++ spec/models/currency_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 require_relative "../../lib/models/currency.rb"
 
 RSpec.describe Currency, type: :model do
-  it "should have a code that complies with ISO 4217" do
-    expect(Currency.new(code: "GBP")).to be_valid
-    expect(Currency.new(code: "GBP1")).to_not be_valid
-    expect(Currency.new(code: "GB")).to_not be_valid
+  context "#code" do
+    it "should be unique" do
+      FactoryBot.create(:currency)
+      expect(FactoryBot.build(:currency)).to_not be_valid
+    end
+
+    it "should comply with ISO 4217" do
+      expect(Currency.new(code: "GBP")).to be_valid
+      expect(Currency.new(code: "GBP1")).to_not be_valid
+      expect(Currency.new(code: "GB")).to_not be_valid
+    end
   end
 end
 
