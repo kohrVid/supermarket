038819be8551b17019b63a8ea07540da313cbbe0
kohrVid <kohrVid@zoho.com>
2020-03-26 19:03:30 +0000

    Add has_many relationship to Item model
    Have added a has_many association with the
    restriction_type_minimum_item_quantity table. This could prove useful if
    there is ever a need to display all deals associated with a particular
    item.

--- lib/models/item.rb
+++ lib/models/item.rb
@@ -1,9 +1,13 @@
 require 'active_record'
+require_relative './restriction_type/minimum_item_quantity.rb'
 
 class Item < ActiveRecord::Base
   belongs_to :currency
   has_many :item_orders
+  has_many :minimum_item_quantity_restrictions,
+    class_name: "RestrictionType::MinimumItemQuantity"
   has_many :orders, through: :item_orders
+
   validates :name, presence: true
   validates :currency_id, presence: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
-require_relative "../../lib/models/item.rb"
+require_relative '../../lib/models/item.rb'
+require_relative '../../lib/models/restriction_type/minimum_item_quantity.rb'
 
 RSpec.describe Item, type: :model do
   let(:item) { FactoryBot.create(:item, :a) }
@@ -12,7 +13,13 @@ RSpec.describe Item, type: :model do
     expect(FactoryBot.build(:item, :a, currency_id: nil)).to_not be_valid
   end
 
-  it "can have many orders" do
-    expect(item).to respond_to(:orders)
+  context "associations" do
+    it "can have many minimum item quantities" do
+      expect(item).to respond_to(:minimum_item_quantity_restrictions)
+    end
+
+    it "can have many orders" do
+      expect(item).to respond_to(:orders)
+    end
   end
 end
