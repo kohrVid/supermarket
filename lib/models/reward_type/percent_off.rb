5082a76509cfef48fe1ab75b1eeaa7fc3957d2b6
kohrVid <kohrVid@zoho.com>
2020-03-27 21:33:24 +0000

    Add a RewardType::PricingRule#apply method
    Have added a method that will apply rewards to a given order. This will
    eventually be called by the PricingRule model and going forward, all
    reward types will be expected to have such a method defined.
    Have also added validation on the percentage to ensure that it isn't
    possible to create "100% off" rewards.

--- lib/models/reward_type/percent_off.rb
+++ lib/models/reward_type/percent_off.rb
@@ -2,5 +2,11 @@ require 'active_record'
 require_relative '../reward_type.rb'
 
 class RewardType::PercentOff < ActiveRecord::Base
-  validates :percentage, presence: true, uniqueness: true
+  validates :percentage, presence: true,
+    uniqueness: true, numericality: { less_than: 100 }
+
+  def apply(order)
+    reduced_total = order.total * (1 - (percentage / 100))
+    order.update(total: reduced_total)
+  end
 end
--- spec/models/reward_type/percent_off_spec.rb
+++ spec/models/reward_type/percent_off_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require_relative "../../../lib/models/item_order.rb"
 require_relative '../../../lib/models/reward_type/percent_off.rb'
 
 RSpec.describe RewardType::PercentOff, type: :model do
@@ -23,6 +24,26 @@ RSpec.describe RewardType::PercentOff, type: :model do
       percent_off
 
       expect(FactoryBot.build(:percent_off)).to_not be_valid
+    end
+
+    it "should be less than 100" do
+      expect(FactoryBot.build(:percent_off, percentage: 100.0)).to_not be_valid
+    end
+  end
+
+  context "apply" do
+    it "should reduce an order total by the correct percentage" do
+      order.items << [item_a, item_b]
+      percent_off.apply(order)
+
+      expect(order.total).to eq(7200)
+    end
+
+    it "should not reduce an order's subtotal" do
+      order.items << [item_a, item_b]
+      percent_off.apply(order)
+
+      expect(order.subtotal).to eq(8000)
     end
   end
 end
