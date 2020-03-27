616d25d4a5cad77bb10aeab2a8b7e3a43bf6d362
kohrVid <kohrVid@zoho.com>
2020-03-27 19:45:21 +0000

    Add associations to the PricingRule model
    The PricingRule model can now have a reward, a restriction group and, by
    extension, many restrictions. With regards to the restrictions
    relationship, it made the most sense to define this in an instance
    method as it isn't possible to perform a map within a scope.

--- lib/models/pricing_rule.rb
+++ lib/models/pricing_rule.rb
@@ -1,5 +1,16 @@
 require 'active_record'
+require_relative './reward.rb'
 
 class PricingRule < ActiveRecord::Base
+  has_one :restriction_group
+  has_one :reward
+  has_many :restrictions, through: :restriction_group
+
   validates :name, presence: true, uniqueness: true
+
+  def restrictions
+    Array(restriction_group).flat_map do |group|
+      group.restrictions.map(&:body)
+    end
+  end
 end
--- spec/models/pricing_rule_spec.rb
+++ spec/models/pricing_rule_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 require_relative "../../lib/models/pricing_rule.rb"
 
 RSpec.describe PricingRule, group: :model do
-  let(:pricing_rule) { FactoryBot.create(:pricing_rule, :line_items) }
+  let(:pricing_rule) do
+    PricingRule.find_or_create_by(attributes_for(:pricing_rule, :line_items))
+  end
 
   it "should be valid if the correct attributes are given" do
     expect(FactoryBot.build(:pricing_rule, :line_items)).to be_valid
@@ -16,6 +18,53 @@ RSpec.describe PricingRule, group: :model do
     it "should be unique" do
       pricing_rule
       expect(FactoryBot.build(:pricing_rule, :line_items)).to_not be_valid
+    end
+  end
+
+  context "associations" do
+    let(:miq_restriction) { FactoryBot.create(:restriction, :miq) }
+    let(:mov_restriction) { FactoryBot.create(:restriction, :mov) }
+    let(:restriction_group) { FactoryBot.create(:restriction_group) }
+    let(:reward) { FactoryBot.create(:reward, :value_off) }
+
+    before do
+      reward
+      restriction_group.restrictions << [miq_restriction, mov_restriction]
+    end
+
+    it "should have a reward" do
+      expect(pricing_rule).to respond_to(:reward)
+      expect(pricing_rule.reward).to be_an_instance_of(Reward)
+    end
+
+    it "should have a restriction group" do
+      expect(pricing_rule).to respond_to(:restriction_group)
+
+      expect(
+        pricing_rule.restriction_group
+      ).to be_an_instance_of(RestrictionGroup)
+    end
+
+    context "#restrictions" do
+      it "can have many restrictions" do
+        expect(pricing_rule).to respond_to(:restrictions)
+      end
+
+      it "can have restrictions of varying types" do
+        expect(
+          pricing_rule.restrictions.first
+        ).to be_an_instance_of(RestrictionType::MinimumItemQuantity)
+
+        expect(
+          pricing_rule.restrictions.second
+        ).to be_an_instance_of(RestrictionType::MinimumOrderValue)
+      end
+
+      it "should return an empty array if no restrictions have been added" do
+        pricing_rule_2 = PricingRule.find_or_create_by(attributes_for(:pricing_rule, :basket))
+
+        expect(pricing_rule_2.restrictions).to be_empty
+      end
     end
   end
 end
