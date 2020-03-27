5ec06177ed67973bfba7aac014346bb9cfa9d24c
kohrVid <kohrVid@zoho.com>
2020-03-27 21:44:22 +0000

    Add RewardType::ValueOff#apply method
    Similarly to the method defined in the PercentOff model, I've added a
    new method that will apply rewards to a given order and discount the
    total accordingly.

--- lib/models/reward_type/value_off.rb
+++ lib/models/reward_type/value_off.rb
@@ -3,4 +3,9 @@ require_relative '../reward_type.rb'
 
 class RewardType::ValueOff < ActiveRecord::Base
   validates :value, presence: true, uniqueness: true
+
+  def apply(order)
+    reduced_total = order.total - value
+    order.update(total: reduced_total)
+  end
 end
--- spec/models/reward_type/value_off_spec.rb
+++ spec/models/reward_type/value_off_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require_relative "../../../lib/models/item_order.rb"
 require_relative '../../../lib/models/reward_type/value_off.rb'
 
 RSpec.describe RewardType::ValueOff, type: :model do
@@ -23,6 +24,22 @@ RSpec.describe RewardType::ValueOff, type: :model do
       value_off
 
       expect(FactoryBot.build(:value_off)).to_not be_valid
+    end
+  end
+
+  context "apply" do
+    it "should reduce an order total by the correct amount" do
+      order.items << [item_a, item_b]
+      value_off.apply(order)
+
+      expect(order.total).to eq(7500)
+    end
+
+    it "should not reduce an order's subtotal" do
+      order.items << [item_a, item_b]
+      value_off.apply(order)
+
+      expect(order.subtotal).to eq(8000)
     end
   end
 end
