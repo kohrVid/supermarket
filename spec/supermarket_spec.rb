f7f74f3047baff691a03a9ed2198e5370382aa11
kohrVid <kohrVid@zoho.com>
2020-03-29 15:18:06 +0100

    Added commands to the Supermarket CLI
    Have added `init`, `display_product`, `scan`, `total` and `receipt`
    commands to the CLI tool. It isn't possible to rely on access to
    instance variables to keep track of changes to the database so I have
    decided to store relevant data (such as the current order, existing
    pricing rules and existing items) in a JSON file. Tests have been added
    to ensure that each of the commands have the correct output though these
    are likely to change in later commits.

---
+++ spec/supermarket_spec.rb
@@ -0,0 +1,128 @@
+require_relative '../supermarket.rb'
+
+RSpec.describe Supermarket, type: :model do
+  let(:pricing_rule) { FactoryBot.create(:pricing_rule, :line_items) }
+  let(:item_a) { FactoryBot.create(:item, :a) }
+  let(:item_b) { FactoryBot.create(:item, :b) }
+  let(:supermarket) { Supermarket.new }
+
+  context "#init" do
+    it "should raise an exception if there are no pricing rules in the database" do
+      expect { supermarket.init }.to raise_error(RuntimeError)
+    end
+
+    it "should create a new order if there are pricing rules present" do
+      pricing_rule
+
+      expect { supermarket.init }.to change{ Order.count }.from(0).to(1)
+    end
+  end
+
+  context "#show_products" do
+    it "should raise an exception if there are no pricing rules in the database" do
+      expect { supermarket.show_products }.to raise_error(RuntimeError)
+    end
+
+    it "should list all products in the database" do
+      item_a
+      item_b
+      pricing_rule
+
+      expect { supermarket.show_products }.to output(
+        "#{{ id: item_a.id, name: item_a.name, price: item_a.price }}\n#{{ id: item_b.id, name: item_b.name, price: item_b.price }}\n"
+      ).to_stdout
+    end
+  end
+
+  context "#scan" do
+    it "should raise an exception if there are no pricing rules in the database" do
+      expect { supermarket.scan(1) }.to raise_error(RuntimeError)
+    end
+
+    context "populated database" do
+      let(:order) { Order.first }
+
+      before do
+        item_a
+        item_b
+        pricing_rule
+        supermarket.init
+      end
+
+      it "should add an item to an order" do
+        expect {
+          supermarket.scan(item_a.id)
+        }.to change{ order.items.count }.from(0).to(1)
+      end
+
+      it "should not add items that don't exist" do
+        expect {
+          supermarket.scan(9999)
+        }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+
+      it "should output new items to stdout" do
+        expect {
+          supermarket.scan(item_a.id)
+        }.to output(/Item #{item_a.name} added to order ##{order.id}/).to_stdout
+      end
+    end
+  end
+
+  context "#total" do
+    it "should raise an exception if there are no pricing rules in the database" do
+      expect { supermarket.total }.to raise_error(RuntimeError)
+    end
+
+    context "populated database" do
+      let(:order) { Order.first }
+
+      before do
+        item_a
+        item_b
+        pricing_rule
+        supermarket.init
+      end
+
+      it "should return 0 if no items have been added" do
+        expect {
+          supermarket.total
+        }.to output(/Total payable is £0.00/).to_stdout
+      end
+
+      it "should return the order total if items have been added" do
+        supermarket.scan(item_a.id)
+
+        expect {
+          supermarket.total
+        }.to output(/Total payable is £50.00/).to_stdout
+      end
+    end
+  end
+
+  context "#receipt" do
+    it "should raise an exception if there are no pricing rules in the database" do
+      expect { supermarket.receipt }.to raise_error(RuntimeError)
+    end
+
+    context "populated database" do
+      let(:order) { Order.first }
+
+      before do
+        item_a
+        item_b
+        pricing_rule
+        supermarket.init
+      end
+
+      it "should return the names of the items added" do
+        supermarket.scan(item_a.id)
+        supermarket.scan(item_b.id)
+
+        expect {
+          supermarket.receipt
+        }.to output(/Order ##{order.id} consists of \[\"A\", \"B\"\]/).to_stdout
+      end
+    end
+  end
+end
--- supermarket.rb
+++ supermarket.rb
@@ -1,9 +1,96 @@
+require 'active_record'
+require 'active_record/base'
+require 'colorize'
+require 'rake'
 require 'thor'
+require_relative './lib/models/checkout.rb'
 
 class Supermarket < Thor
-  desc "hello NAME", "say hello to NAME"
-  def hello(name)
-    puts "Hello #{name}"
+  db = YAML.load(File.read("#{Dir.pwd}/db/config.yml"))["development"]
+  ActiveRecord::Base.establish_connection(db)
+  TMP_DATA = "#{Dir.pwd}/tmp/cli-data.json"
+
+  desc "init", "Initialise the app and with default rows"
+  def init
+    check_pricing_rules_exist
+    pricing_rules = PricingRule.all
+    checkout = Checkout.new(pricing_rules)
+
+    puts "Order ##{checkout.order.id} created. Please scan the items that you would like to purchase"
+      .colorize(:green)
+
+    update_tmp_data(checkout, pricing_rules)
+  end
+
+  desc "show_products", "List all items available in the supermarket"
+  def show_products
+    check_pricing_rules_exist
+    puts Item.all.map{|item| {id: item.id, name: item.name, price: item.price} }
+  end
+
+  desc "scan", "Scan an item in the supermarket"
+  def scan(item_id)
+    check_pricing_rules_exist
+    (order, pricing_rules, checkout) = database_variables
+
+    item = Item.find(item_id)
+    checkout.scan(item)
+
+    puts "Item #{item.name} added to order ##{checkout.order.id}"
+      .colorize(:green)
+
+    update_tmp_data(checkout, pricing_rules)
+  end
+
+  desc "total", "Return the basket total"
+  def total
+    check_pricing_rules_exist
+    (order, pricing_rules, checkout) = database_variables
+
+    total = checkout.total
+    puts "Total payable is #{total}".colorize(:green)
+    update_tmp_data(checkout, pricing_rules)
+  end
+
+  desc "receipt", "Show receipt"
+  def receipt
+    check_pricing_rules_exist
+    (order, pricing_rules, checkout) = database_variables
+
+    puts "Order ##{checkout.order.id} consists of #{order.items.map(&:name)}"
+      .colorize(:green)
+  end
+
+  private
+
+  def check_pricing_rules_exist
+    if PricingRule.count < 1
+      raise "Please run `rake db:seed` to populate the database first"
+        .colorize(:red)
+    end
+  end
+
+  def update_tmp_data(checkout, pricing_rules)
+    File.open(TMP_DATA, "w+") do |f|
+      f.write(
+        {
+          "order": checkout.order,
+          "pricing_rules": pricing_rules.map(&:serializable_hash)
+        }.to_json
+      )
+    end
+  end
+
+  def database_variables
+    order = Order.where(
+      JSON.parse(File.read(TMP_DATA))["order"]
+        .symbolize_keys.slice(:id, :subtotal, :total, :currency_id)
+    ).first
+
+    pricing_rules = PricingRule.all
+    checkout = Checkout.new(pricing_rules, order)
+
+    [order, pricing_rules, checkout]
   end
 end
 
